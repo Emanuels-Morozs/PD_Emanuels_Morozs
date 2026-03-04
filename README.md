97a42f3ea91d37fec5a2f3cfb3ffda5947487299
Emanuels-Morozs <sela_popaja@inbox.lv>
2026-03-04 14:38:56 +0200

    uzrakstīta funkcija datu ievadīšanai

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # PD_Emanuels_Morozs
 # Apraksts
+Šī programma:
+    prasa lietotāju ievadīt datus
+    aprēķina masu, izmaksas
 # Palaišanas instrukcija
+Lejupielādē failu programmu
+Palaid Python vidē
+Ievadi prasītus datus
 # Licence
+MIT License
--- uzdevums.py
+++ uzdevums.py
@@ -1,6 +1,10 @@
 def ievade():
-
+    cementa_masa=float(input("Ievadiet cementa masu: "))
+    cementa cena=float(input("Ievadiet cementa cenu par kilogramu: "))
+    smilsu_cena=float(input("Ievadiet smilšu cenu: "))
+    if cementa_masa or cementa_cena or smilsu_cena == 0:
+        print("Kļūda")
 def masa():
-
+    1234567
 def izmaksas():
 
